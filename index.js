9196d41d6458c59140c28fbb4314dcda163d1e0d
montahefr <montahaferjeni@gmail.com>
2025-03-18 17:43:06 +0100

    newer version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const db = require('./config/db')
 const UserModel =require('./model/user.model')
-const port =3000;
+const port =5000;
 
 app.get('/',(req,res)=>{
     res.send("Hello World!!")
